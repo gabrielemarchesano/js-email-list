ff84b1f5446057c53907754d83dc6e689979a349
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-23 14:09:55 +0200

    try to log the response of the boolean endpoint

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+
+/* Utilizzo libreria Axios */
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((risposta) => {
+    //stampo in console la risposta dell'endpoint
+    console.log(risposta);        
+  })
