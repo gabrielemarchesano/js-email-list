1fbbb465c58d30133afa7709fc9f6feae388daea
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-23 14:16:36 +0200

    print on console 10 random mails

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
 /* Utilizzo libreria Axios */
-
+for(let i = 0; i < 10; i++){
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((risposta) => {
     //stampo in console la risposta dell'endpoint
-    console.log(risposta);        
-  })
+    console.log(risposta.data);
+    }
+  )
+}
