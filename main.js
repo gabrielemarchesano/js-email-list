2b79cf5d35054f9a6ff63fe5edd70678622019cb
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-23 14:43:14 +0200

    select html element, get the key value, print in the html page

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
-/* Utilizzo libreria Axios */
+// seleziono l'elemento della DOM
+const listElement = document.getElementById("mail-list");
+
+// creo una variabile vuota
+let items = "";
+
+// ciclo per 10 volte così da generare 10 risposte
 for(let i = 0; i < 10; i++){
+// Utilizzo libreria Axios per
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((risposta) => {
-    //stampo in console la risposta dell'endpoint
-    console.log(risposta.data);
-    }
+    const object = risposta.data;
+    //stampo in console la risposta dell'endpoint, il tipo per controllare se funziona
+    console.log("Prova:", typeof object, object);
+    // concateno i list items
+    items += `<li>${object.response}</li>`;
+    // modifico il markup con i list items
+    listElement.innerHTML = items;
+  }
   )
 }
