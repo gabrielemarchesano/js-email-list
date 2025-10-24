d0cf96a76710dc9933779f7d26e358c2109226bf
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-24 07:28:32 +0200

    bonus2: add button element and event to reset the list

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 // seleziono l'elemento della DOM
 const listElement = document.getElementById("mail-list");
+const buttonElement = document.getElementById("reset-list");
 
 // creo una variabile vuota
 let items = "";
@@ -25,3 +26,21 @@ axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   }
   )
 }
+
+buttonElement.addEventListener("click", () => {
+  listElement.innerHTML = "";
+  for(let i = 0; i < 10; i++){
+  // Utilizzo libreria Axios per
+  axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((risposta) => {
+      const object = risposta.data;
+      //stampo in console la risposta dell'endpoint, il tipo per controllare se funziona
+      console.log("Prova:", typeof object, object);
+      // concateno i list items
+      items = `<li>${object.response}</li>`;
+      // modifico il markup con i list items
+      listElement.innerHTML += items;
+    }
+    )
+  }
+})
