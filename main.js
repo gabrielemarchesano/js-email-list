1397b3d885805a523d188ec6473ba9b62b0bae4e
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-24 08:33:21 +0200

    bonus2: add comments to better explain the code

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let items = "";
 
 // ciclo per 10 volte così da generare 10 risposte
 for(let i = 0; i < 10; i++){
-// Utilizzo libreria Axios per
+// Utilizzo libreria Axios per richiedere la mail
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((risposta) => {
     const object = risposta.data;
@@ -27,18 +27,21 @@ axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   )
 }
 
+// evento al click che chiederà al server una risposta
 buttonElement.addEventListener("click", () => {
+  // svuoto il contenuto della lista
   listElement.innerHTML = "";
+  // ciclo 10 volte per generare 10 richieste
   for(let i = 0; i < 10; i++){
-  // Utilizzo libreria Axios per
+  // Utilizzo libreria Axios per richiedere la mail
   axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((risposta) => {
       const object = risposta.data;
       //stampo in console la risposta dell'endpoint, il tipo per controllare se funziona
       console.log("Prova:", typeof object, object);
-      // concateno i list items
+      // creo un elemento <li> che contiene la mail
       items = `<li>${object.response}</li>`;
-      // modifico il markup con i list items
+      // modifico il markup concatenando il singolo item ad ogni iterazione
       listElement.innerHTML += items;
     }
     )
